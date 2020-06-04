764d2826631294cad986c983048fa53a662ccbae
adeelibr <adeelimranr@gmail.com>
2020-06-04 22:04:18 +0200

    add comments to todo model functions

--- chapter_2:mysql/models/todo.ts
+++ chapter_2:mysql/models/todo.ts
@@ -5,6 +5,12 @@ import { TABLE } from "../db/config.ts";
 import Todo from "../interfaces/Todo.ts";
 
 export default {
+  /**
+   * Takes in the id params & checks if the todo item exists
+   * in the database
+   * @param id
+   * @returns boolean to tell if an entry of todo exits in table
+   */
   doesExistById: async ({ id }: Todo) => {
     const [result] = await client.query(
       `SELECT COUNT(*) count FROM ${TABLE.TODO} WHERE id = ? LIMIT 1`,
@@ -12,15 +18,30 @@ export default {
     );
     return result.count > 0;
   },
+  /**
+   * Will return all the entries in the todo column
+   * @returns array of todos
+   */
   getAll: async () => {
     return await client.query(`SELECT * FROM ${TABLE.TODO}`);
   },
+  /**
+   * Takes in the id params & returns the todo item found
+   * against it.
+   * @param id
+   * @returns object of todo item
+   */
   getById: async ({ id }: Todo) => {
     return await client.query(
       `SELECT * FROM ${TABLE.TODO} WHERE id = ?`,
       [id],
     );
   },
+  /**
+   * Adds a new todo item to todo table
+   * @param todo
+   * @param isCompleted
+   */
   add: async (
     { todo, isCompleted }: Todo,
   ) => {
@@ -32,6 +53,13 @@ export default {
       ],
     );
   },
+  /**
+   * Updates the content of a single todo item
+   * @param id
+   * @param todo
+   * @param isCompleted
+   * @returns integer (count of effect rows)
+   */
   updateById: async ({ id, todo, isCompleted }: Todo) => {
     const result = await client.query(
       `UPDATE ${TABLE.TODO} SET todo=?, isCompleted=? WHERE id=?`,
@@ -44,6 +72,11 @@ export default {
     // return count of rows updated
     return result.affectedRows;
   },
+  /**
+   * Deletes a todo by ID
+   * @param id
+   * @returns integer (count of effect rows)
+   */
   deleteById: async ({ id }: Todo) => {
     const result = await client.query(
       `DELETE FROM ${TABLE.TODO} WHERE id = ?`,
